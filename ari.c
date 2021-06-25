c96ad80fe2e40579b5327819ba98cf5a97afe38a
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-25 22:15:23 +0530

    Update ari.c

---
+++ ari.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
+
+string ari(string s){
+int count_characters=0,count_words=0,count_sentences=0;
+for(int i=0;i<s.length();i++){
+if(isalnum(s[i])
+count_characters++;
+if(s[i]==' ')
+count_words++;
+if(s[i]=='.')
+count_sentences++;
+}
+float score=4.71*((float)count_characters/count_words)+0.5*((float)count_words/count_sentences)-21.43;
+int ari_score=ceil(score);
+printf("%d",ari_score);
+return "hello";
+}
