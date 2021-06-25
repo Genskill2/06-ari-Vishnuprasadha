1b5b8bc4dcf5f78e1fc91b3e20f8d47eeade4224
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-25 22:25:21 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -17,6 +17,33 @@ count_sentences++;
 }
 float score=4.71*((float)count_characters/count_words)+0.5*((float)count_words/count_sentences)-21.43;
 int ari_score=ceil(score);
-printf("%d",ari_score);
-return "hello";
+  if(ari_score==1)
+   return "Kindergarten"
+   else if(ari_score==2)
+   return "First/Second Grade"
+   else if(ari_score==3)
+   return "Third Grade"
+   else if(ari_score==4)
+   return "Fourth Grade"
+   else if(ari_score==5)
+   return "Fifth Grade"
+   else if(ari_score==6)
+   return "Sixth Grade"
+   else if(ari_score==7)
+   return "Seventh Grade"
+   else if(ari_score==8)
+   return "Eighth Grade"
+   else if(ari_score==9)
+   return "Ninth Grade"
+   else if(ari_score==10)
+   return "Tenth Grade"
+   else if(ari_score==11)
+   return "Eleventh Grade"
+   else if(ari_score==12)
+   return "Twelfth grade"
+   else if(ari_score==13)
+   return "College student"
+   else (ari_score==14)
+   return "Professor"
+   
 }
